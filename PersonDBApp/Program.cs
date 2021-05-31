ebd2a7178ef0a5747e4a617e5c30f9e69281468c
Tatu Paski <tatupaski@outlook.com>
2021-05-31 12:02:12 +0300

    Made PersonDBApp

--- PersonDBApp/Program.cs
+++ PersonDBApp/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using PersonDBApp.Data;
 
 namespace PersonDBApp
 {
     class Program
     {
+        static private readonly PersonTestDBContext context = new PersonTestDBContext();
         static void Main(string[] args)
         {
            
